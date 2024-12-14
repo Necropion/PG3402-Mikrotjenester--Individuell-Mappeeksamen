7e424a0e7d14e10fdb80d8a4a29fb22a0bdbfb2e
jokas3@gmail.com <jokubasgedminas@MacBookPro.int.gedminai.no>
2024-12-14 23:48:44 +0100

    Added creating new cart functionality

--- Client/View/Components/CartList.jsx
+++ Client/View/Components/CartList.jsx
@@ -28,6 +28,25 @@ const CartList = ({ setCart }) => {
     const handleClick = async (e) => {
         e.preventDefault();
 
+        if(e.target.id === "createBtn") {
+            const createCart = await fetch(`${gateway}/api/cart`, {
+                method: "POST",
+                body: JSON.stringify({userId: user.id}),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+            const newCart = await createCart.json();
+
+            if(createCart.ok) {
+                setCart(newCart);
+                setCartSelected(true);
+                console.log(newCart);
+            }
+
+            console.log("Failed to create a new cart");
+        }
+
         if(e.target.id === "deleteBtn") {
             const deleteCartById = await fetch(`${gateway}/api/cart/${e.target.dataset.cartId}`, {
                 method: 'DELETE'
@@ -54,13 +73,15 @@ const CartList = ({ setCart }) => {
 
     return (
         <div className={'bg-white rounded-xl ml-[1vw] w-[25vw] max-h-[400px] flex flex-col justify-center'}>
-            {cartList ? cartList.map((cart, index) => (
-                <div key={index} className={'h-[50px] text-xl flex flex-wrap'}>
-                    <div>Cart ID: {cart.id} User ID: {cart.userId}</div>
-                    <button id="deleteBtn" data-cart-id={cart.id} onClick={handleClick}>Delete</button>
-                    <button id="selectBtn" data-cart-id={cart.id} onClick={handleClick}>Select</button>
+            <div className={'flex-1 h-[80%]'}>{cartList ? cartList.map((cart, index) => (
+                <div key={index} className={'h-[50px] text-xl flex flex-wrap content-center'}>
+                    <div className={'pl-5 w-[55%]'}>Cart {index + 1}</div>
+                    <button id="deleteBtn" data-cart-id={cart.id} onClick={handleClick} className={'w-[20%]'}>Delete</button>
+                    <button id="selectBtn" data-cart-id={cart.id} onClick={handleClick} className={'w-[20%]'}>Select</button>
                 </div>
             )) : <div className={'text-2xl'}>Add an item to the cart to begin.</div>}
+            </div>
+            <button id="createBtn" onClick={handleClick} className={'text-2xl'}>New Cart</button>
         </div>
     )
 }
